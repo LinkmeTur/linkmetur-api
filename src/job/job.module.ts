0a1f03e842b259211abe4f0669118c12f5f4b5fe
Jamerson Felipe Pereira Paz <ljames666@gmail.com>
2025-05-30 11:49:55 -0300

    ci: v1 cr

--- src/job/job.module.ts
+++ src/job/job.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { JobService } from './job.service';
 import { JobsController } from './job.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Job } from './entities/job.entity';
+import { JobPhotos } from './entities/job_photos.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Job, JobPhotos])],
   controllers: [JobsController],
   providers: [JobService],
+  exports: [TypeOrmModule, JobService],
 })
 export class JobModule {}
