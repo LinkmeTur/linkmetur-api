17f1337984fb21d83c1d1adf492e8cf06c6a36c3
Jamerson Felipe Pereira Paz <ljames666@gmail.com>
2025-08-14 03:19:34 -0300

    foi

--- src/request-for-proposal/dto/update-request-for-proposal.dto.ts
+++ src/request-for-proposal/dto/update-request-for-proposal.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateRequestForProposalDto } from './create-request-for-proposal.dto';
 
-export class UpdateRequestForProposalDto extends PartialType(CreateRequestForProposalDto) {}
+export class UpdateRequestForProposalDto extends PartialType(
+  CreateRequestForProposalDto,
+) {}
